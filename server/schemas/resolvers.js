7a0de6ec79b73b4612af838bba1746a5d308ad33
Kerry <kerryhokanson@gmail.com>
2023-02-22 20:11:38 -0600

    resolvers built

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,23 +3,29 @@ const {User, Book} = require('../models')
 
 const resolvers = {
     Query: {
-        getSingleUser: async () => {
-            const foundUser = await User.findOne({
+        getSingleUser: async (parent, {user}) => {
+            return await User.findOne({
                 $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
               });
         }
     },
     Mutation: {
-        createUser: async () => {
-
+        createUser: async (parent, {body}) => {
+            const user = await User.create(body);
+            return user
         },
-        saveBook: async () => {
-            
-        },
-        deleteBook: async (parent, {user, params}) => {
+        saveBook: async (parent, {user}) => {
             const updatedUser = await User.findOneAndUpdate(
                 { _id: user._id },
-                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+              );
+            return updatedUser;
+        },
+        deleteBook: async (parent, {user, bookId}) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
                 { new: true }
               );
         }
